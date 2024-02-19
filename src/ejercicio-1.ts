2ec8c037f42f4fbb51454cdf166e02c5057bcf02
alu0101316734 <alu0101316734@ull.edu.es>
2024-02-19 22:17:00 +0000

    Ejercicio 1 hecho

--- src/ejercicio-1.ts
+++ src/ejercicio-1.ts
@@ -7,12 +7,31 @@ class Gestor_de_Referncias_Bibliograficas {
     constructor(readonly titulo: string, readonly autor: string,readonly palabras_claves: string[], readonly resumen: string,readonly fecha: Date,readonly n_paginas: number,readonly editorial: string) {
         this.recopilacion.push([titulo, autor, palabras_claves, resumen, fecha, n_paginas, editorial]); 
     }
-    add_trabajo(titulo: string, autor: string,palabras_claves: string[], resumen: string,fecha: Date,n_paginas: number, editorial: string){
+    add_trabajo(titulo: string, autor: string,palabras_claves: string[], resumen: string,fecha: Date,n_paginas: number, editorial: string):void{
         this.recopilacion.push([titulo, autor, palabras_claves, resumen, fecha, n_paginas, editorial])
+    }
+    getrecopilacion():trabajos{
+        return this.recopilacion;
     }
 };
 
 
 class Articulo extends Gestor_de_Referncias_Bibliograficas{
-  
+   private revista:string[] =[];
+   private volumen:string[] = [];
+   constructor(readonly titulo: string, readonly autor: string,readonly palabras_claves: string[], readonly resumen: string,readonly fecha: Date,readonly n_paginas: number,readonly editorial: string,volumen:string,revista:string){
+     super(titulo,autor,palabras_claves,resumen,fecha,n_paginas,editorial);
+     this.revista.push(revista); 
+     this.volumen.push(volumen);
+   }
+   add_trabajo_Articulo(titulo: string, autor: string, palabras_claves: string[], resumen: string, fecha: Date, n_paginas: number, editorial: string,volumen:string,revista:string): void {
+       super.add_trabajo(titulo,autor,palabras_claves,resumen,fecha,n_paginas,editorial);
+       this.volumen.push(volumen);
+       this.revista.push(revista);
+       return;
+   }
+   print():void
+   {
+     console.table(super.getrecopilacion()+this.revista,,this.volumen)
+   }
 }
